03531633afa394e8fd8b7b2bea409bb0fbbc1f9c
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2023-12-05 17:19:46 +0300

    Created AccountController class.

---
+++ src/main/java/com/springboot/accounting/controller/AccountController.java
@@ -0,0 +1,25 @@
+package com.springboot.accounting.controller;
+
+import com.springboot.accounting.dto.AccountDto;
+import com.springboot.accounting.dto.CreateAccountRequest;
+import com.springboot.accounting.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/account")
+//@RequestMapping(value = "/v1/account", method = RequestMethod.POST)
+public class AccountController {
+
+    private final AccountService accountService;
+
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountDto> createAccount(@RequestBody CreateAccountRequest request){
+        return ResponseEntity.ok(accountService.createAccount(request));
+    }
+}
