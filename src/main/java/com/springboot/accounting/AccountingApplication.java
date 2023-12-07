cbb6f5b9eff618b17d64622c821ae6af0a2dcedf
memmir <mahmutekilinc@gmail.com>
2023-12-07 11:18:40 +0300

    Created instance on main class

--- src/main/java/com/springboot/accounting/AccountingApplication.java
+++ src/main/java/com/springboot/accounting/AccountingApplication.java
@@ -1,13 +1,30 @@
 package com.springboot.accounting;
 
+import com.springboot.accounting.model.Customer;
+import com.springboot.accounting.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+
 @SpringBootApplication
-public class AccountingApplication {
+public class AccountingApplication implements CommandLineRunner {
+
+	private final CustomerRepository customerRepository;
+
+	public AccountingApplication(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountingApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		Customer customer = customerRepository.save(new Customer("Cagri", "Dursun"));
+		System.out.println(customer);
 	}
 
 }
