8bd2ad422e2b33e47f02edad7ce5d1e984c1e1b4
memmir <mahmutekilinc@gmail.com>
2023-12-08 14:34:02 +0300

    Customer Controller class created.

---
+++ src/main/java/com/springboot/accounting/controller/CustomerController.java
@@ -0,0 +1,24 @@
+package com.springboot.accounting.controller;
+
+import com.springboot.accounting.dto.CustomerDto;
+import com.springboot.accounting.service.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/customer")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<CustomerDto> GetCustomerById(@PathVariable String customerId){
+        return ResponseEntity.ok(customerService.getCustomerById(customerId));
+    }
+}
