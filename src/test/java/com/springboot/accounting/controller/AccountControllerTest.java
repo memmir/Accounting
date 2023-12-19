7536dae370f0103087d975a39a2118793e279bca
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2023-12-19 22:56:46 +0300

    Created AccountControllerTest for Integration test

---
+++ src/test/java/com/springboot/accounting/controller/AccountControllerTest.java
@@ -0,0 +1,22 @@
+package com.springboot.accounting.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+        "server-port=0",
+        "command.line.runner.enabled=false"
+})
+@RunWith(SpringRunner.class)
+@DirtiesContext
+class AccountControllerTest {
+
+}
