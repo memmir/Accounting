d371dbb19aad288d3891aa328bd423819e94f368
memmir <mahmutekilinc@gmail.com>
2023-12-20 11:01:36 +0300

    AccountController Integration test class initializations.

--- src/test/java/com/springboot/accounting/controller/AccountControllerTest.java
+++ src/test/java/com/springboot/accounting/controller/AccountControllerTest.java
@@ -1,13 +1,26 @@
 package com.springboot.accounting.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.springboot.accounting.dto.AccountDtoConverter;
+import com.springboot.accounting.repository.AccountRepository;
+import com.springboot.accounting.service.AccountService;
+import com.springboot.accounting.service.CustomerService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.Clock;
+import java.util.UUID;
+import java.util.function.Supplier;
 
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
@@ -18,5 +31,37 @@ import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
 @DirtiesContext
 class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private Clock clock;
+
+    @MockBean
+    private Supplier<UUID> uuidSupplier;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private AccountDtoConverter accountDtoConverter;
+
+    private AccountService accountService = new AccountService(accountRepository, customerService, accountDtoConverter, clock);
+    private ObjectMapper mapper = new ObjectMapper();
+
+    private static final UUID uuid = UUID.randomUUID();
+
+    @BeforeEach
+    public void setup(){
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+    }
+
+
 
 }
