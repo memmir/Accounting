70ee99c993e392fc7ffd2fe1b3db5e7bcb2f76a6
memmir <mahmutekilinc@gmail.com>
2023-12-20 11:02:03 +0300

    Added clock variable.

--- src/main/java/com/springboot/accounting/service/AccountService.java
+++ src/main/java/com/springboot/accounting/service/AccountService.java
@@ -10,18 +10,25 @@ import com.springboot.accounting.repository.AccountRepository;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.UUID;
+import java.util.function.Supplier;
 
 @Service
 public class AccountService {
     private final AccountRepository accountRepository;
     private final CustomerService customerService;
     private final AccountDtoConverter accountDtoConverter;
+    private final Clock clock;
 
-    public AccountService(AccountRepository accountRepository, CustomerService customerService, AccountDtoConverter accountDtoConverter) {
+    public AccountService(AccountRepository accountRepository, CustomerService customerService, AccountDtoConverter accountDtoConverter, Clock clock) {
         this.accountRepository = accountRepository;
         this.customerService = customerService;
         this.accountDtoConverter = accountDtoConverter;
+        this.clock = clock;
+
     }
 
     public AccountDto createAccount(CreateAccountRequest createAccountRequest){
@@ -30,7 +37,7 @@ public class AccountService {
         Account account = new Account(
                 customer,
                 createAccountRequest.getInitialCredit(),
-                LocalDateTime.now());
+                getLocalDateTimeNow());
 
         if(createAccountRequest.getInitialCredit().compareTo(BigDecimal.ZERO) > 0){
             //Transaction transaction = transactionService.initiateMoney(account, createAccountRequest.getInitialCredit());
@@ -42,5 +49,11 @@ public class AccountService {
         Account newAccount = accountRepository.save(account);
         return accountDtoConverter.convert(newAccount);
     }
+    private LocalDateTime getLocalDateTimeNow(){
+        Instant instant = clock.instant();
+        return LocalDateTime.ofInstant(instant, Clock.systemDefaultZone().getZone());
+
+    }
+
 
 }
