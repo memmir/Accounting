9245f15b91a8489d620a9f1f38d678011ad31c3c
memmir <mahmutekilinc@gmail.com>
2023-12-22 17:27:53 +0300

    First Try with React.

--- accounting-fe/src/App.js
+++ accounting-fe/src/App.js
@@ -1,8 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    customer: []
+  }
+
+  async componentDidMount() {
+    const response = await fetch('/v1/customer/72bd89cf-6510-49be-99bc-7dc3e13bd7b6');
+    const body = await response.json();
+    this.setState({customer: body});
+  }
+  render() {
+    const {customer} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>customer</h2>
+                  <div key={customer.id}>
+                    {customer.name} {customer.surname}
+                  </div>
+            </div>
+          </header>
+        </div>
+    );
+  }
+  /*return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +45,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  );*/
 }
 
 export default App;
